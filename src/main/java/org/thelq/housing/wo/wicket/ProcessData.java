46d9bc036b997f5be2c8c5728d7f538fb19e10d9
Leon Blakey <lord.quackstar@gmail.com>
2012-09-13 09:35:51 -0400

    Initial working response from a response

--- src/main/java/org/thelq/housing/wo/wicket/ProcessData.java
+++ src/main/java/org/thelq/housing/wo/wicket/ProcessData.java
@@ -5,15 +5,28 @@
  */
 package org.thelq.housing.wo.wicket;
 
-import org.apache.wicket.markup.html.WebPage;
-import org.apache.wicket.markup.html.basic.Label;
+import org.apache.wicket.request.resource.AbstractResource;
+import org.apache.wicket.request.resource.AbstractResource.ResourceResponse;
+import org.apache.wicket.request.resource.AbstractResource.WriteCallback;
+import org.apache.wicket.request.resource.IResource.Attributes;
 
 /**
  * Handle input
+ *
  * @author Leon Blakey <lord.quackstar at gmail.com>
  */
-public class ProcessData extends WebPage {
-	public ProcessData() {
-		add(new Label("test", "Hello World!"));
+public class ProcessData extends AbstractResource {
+	private static final long serialVersionUID = 1L;
+
+	@Override
+	protected ResourceResponse newResourceResponse(Attributes a) {
+		ResourceResponse r = new ResourceResponse();
+		r.setContentType("application/json");
+		r.setWriteCallback(new WriteCallback() {
+			public void writeData(Attributes a) {
+				a.getResponse().write("[{\"id\":111,\"title\":\"MainEvent\",\"start\":\"2012-04-10T07:00:00\",\"end\":\"2012-04-10T09:30:00\",\"url\":\"?EventID=111\",\"allDay\":false}]");
+			}
+		});
+		return r;
 	}
 }
