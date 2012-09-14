c14de1da808ffd2ebdec99a9ae3d40ac6a07e872
Leon Blakey <lord.quackstar@gmail.com>
2012-09-14 12:52:21 -0400

    Migrated to commons-lang3

--- src/main/java/org/thelq/housing/wo/wicket/Spreadsheet.java
+++ src/main/java/org/thelq/housing/wo/wicket/Spreadsheet.java
@@ -29,7 +29,7 @@ import java.util.Map;
 import java.util.Properties;
 import lombok.Data;
 import lombok.Getter;
-import org.apache.commons.lang.StringUtils;
+import org.apache.commons.lang3.StringUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
