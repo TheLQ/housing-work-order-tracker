a1d79ba72cbaa769cdb23e6b2734d94f1d05cbd9
Leon Blakey <lord.quackstar@gmail.com>
2012-11-22 19:00:35 -0500

    Finished writing all existing issue logic, just need tuning

--- src/main/webapp/js/form-ajax.js
+++ src/main/webapp/js/form-ajax.js
@@ -7,11 +7,11 @@ $(document).ready(function(){
 	//Dynamic room lookup
 	$("#room").live('input', function() {
 		room = $(this).val();
-					
+
 		//Ignore rooms with less than 3 characters, user is probably typing in a room
 		if(room.length < 3)
 			return;
-					
+
 		building = $("#building option:selected").val();
 		if(building == "")
 			$("#roomStatus").html("No building is selected!");
@@ -20,11 +20,11 @@ $(document).ready(function(){
 			$("#roomStatus").html("Fetching info for " + building + " " + room + "...");
 			$.ajaxSetup ({
 				cache: false
-			}); 
+			});
 			$.getJSON("processData?mode=room", {
-				building: building, 
+				building: building,
 				room: room
-			}, function(json) {  
+			}, function(json) {
 				if(typeof json.error != 'undefined')
 					$("#roomStatus").html("Server Error! " + json.error);
 				else {
@@ -32,14 +32,14 @@ $(document).ready(function(){
 					$("#roomStatus").html(json.response)
 					woUtils.inject(json.data);
 				}
-			}  
-			);  
+			}
+			);
 		}
 	});
-				
+
 	//Automagical AJAX submit
 	$("#mainForm").ajaxForm({
-		//target:        '#submitStatus',   // target element(s) to be updated with server response 
+		//target:        '#submitStatus',   // target element(s) to be updated with server response
 		beforeSerialize: function() {
 			$(".issueBox").each(function() {
 				curStatus = $(".statusName", $(this)).html();
@@ -48,7 +48,7 @@ $(document).ready(function(){
 		},
 		beforeSubmit: function() {
 			$("#submitStatus").html("Submitting...");
-			
+
 			/* Form Checks */
 			if($("#building").val().length == 0) {
 				alert("You haven't selected a building!")
@@ -58,7 +58,7 @@ $(document).ready(function(){
 				alert("You haven't selected a room!")
 				return false;
 			}
-			
+
 			allGood = true;
 			$(".note").each(function() {
 				//See if the length is 0 and the note isn't the first one in the issue
@@ -92,12 +92,37 @@ $(document).ready(function(){
 		error: function(data) {
 			$("#submitStatus").html("Javascript Error! " + data.responseText);
 		},
- 
-		// other available options: 
-		//url:       "processData?mode=form",         // override for form's 'action' attribute 
-		//type:      "POST",        // 'get' or 'post', override for form's 'method' attribute 
-		dataType:  'json'        // 'xml', 'script', or 'json' (expected server response type) 
-	//clearForm: true,        // clear all form fields after successful submit 
-	//resetForm: true        // reset the form after successful submit 
+
+		// other available options:
+		//url:       "processData?mode=form",         // override for form's 'action' attribute
+		//type:      "POST",        // 'get' or 'post', override for form's 'method' attribute
+		dataType:  'json'        // 'xml', 'script', or 'json' (expected server response type)
+	//clearForm: true,        // clear all form fields after successful submit
+	//resetForm: true        // reset the form after successful submit
 	});
+
+	//Existing issue handling
+	function handleExisting(building) {
+		$("#existingStatus").html("Loading...")
+
+		$.ajaxSetup ({
+			cache: false
+		});
+		$.getJSON("processData?mode=existing", {
+			building: building
+		}, function(json) {
+			if(typeof json.error != 'undefined')
+				$("#existingStatus").html("Server Error! " + json.error);
+			else {
+				console.log("Finished querying exisitng issues")
+				$("#existingStatus").html(json.response)
+				woUtils.injectExisting(json.data);
+			}
+		}
+		);
+	}
+	$("#existBuilding").on("change", function() {
+		handleExisting($(this).val())
+	})
+	handleExisting($("#existBuilding").val())
 });
---
+++ src/main/webapp/js/form-existing.js
@@ -0,0 +1,88 @@
+/*
+ * Copyright (C) 2012 Leon Blakey <lord.quackstar at gmail.com>, RATS 2012
+ *
+ * This file is part of University of Louisville Housing WorkOrder System.
+ */
+$(document).ready(function(){
+	/**
+	 * Script to handle the existing issue part of the form
+	 */
+	existingContainer = $("#existingContainer")
+
+	woUtils.injectExisting = function(data) {
+		woUtils.resetExisting()
+		for(var location in data) {
+			console.info("Starting to parse existing issues at " + location)
+			lastExistingBox = $(".existingBox", existingContainer).last()
+
+			//Create a new existingIssueBox if the last one has data in it
+			if($(".existingSheetId", lastExistingBox).html().length != 0) {
+				newExistingBox = lastExistingBox.clone()
+				newExistingBox.insertAfter(lastExistingBox)
+				woUtils.resetExistingIssue(newExistingBox)
+				lastExistingBox = newExistingBox
+
+			}
+
+			//Show location
+			$(".existingLocation", lastExistingBox).html(location);
+
+			//Parse each issue that is under this location
+			locationData = data[location]
+
+			for(i in locationData) {
+				console.debug("Parsing existing issue #" + i + " at location " + location)
+
+				//Get the last existingIssueBox or create a new one if there's already data
+				lastExistingIssueBox = $(".existingIssueBox", lastExistingBox).last();
+				if($(".existingSheetId", lastExistingIssueBox).html().length != 0) {
+					newExistingIssueBox = lastExistingIssueBox.clone();
+					newExistingIssueBox.insertAfter(lastExistingIssueBox)
+					lastExistingIssueBox = newExistingIssueBox;
+				}
+
+				$(".existingSheetId", lastExistingIssueBox).html(locationData[i]["sheetId"])
+				$(".existingIssue", lastExistingIssueBox).html(locationData[i]["issue"])
+				$(".existingOpened", lastExistingIssueBox).html(locationData[i]["opened"])
+				$(".existingWaiting", lastExistingIssueBox).html(locationData[i]["closed"])
+
+				//Set status
+				$(".existingStatus", lastExistingIssueBox).html(locationData[i]["status"])
+				if(locationData[i]["status"] == "Open")
+					$(".existingStatus", lastExistingIssueBox).addClass("label-important")
+				else if(locationData[i]["status"] == "Waiting")
+					$(".existingStatus", lastExistingIssueBox ).addClass("label-warning")
+				else
+					alert("Unknown status " + locationData[i]["status"] + " contained at position " + location)
+			}
+		}
+	}
+
+	woUtils.resetExistingIssue = function(existingBox) {
+		//Reset to default values
+		$(".existingWaitingWrap", existingBox).hide();
+		$(".existingLocation", existingBox).html("");
+		$(".existingSheetId", existingBox).html("")
+		$(".existingStatus", existingBox).html("")
+		$(".existingStatus", existingBox).removeClass("label-important label-warning label-success")
+		$(".existingIssue", existingBox).html("")
+		$(".existingOpened", existingBox).html("")
+		$(".existingWaiting", existingBox).html("")
+
+		existingBox.children(".existingIssueBox").each(removeAllButFirst);
+	}
+
+	woUtils.resetExisting = function() {
+		//Remove all existing existingBox's and existingIssueBox's except the first one
+		existingContainer.children(".existingBox").each(removeAllButFirst);
+
+		//Reset remaining issue
+		woUtils.resetExistingIssue($(".existingBox", existingContainer))
+	}
+
+	function removeAllButFirst(i) {
+		if(i == 0)
+			return;
+		$(this).remove()
+	}
+});
